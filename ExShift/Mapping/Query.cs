c632431a601468db6217da61717614d70e9e36a2
jbaumann2 <jan.baumann2@outlook.com>
2021-01-29 00:01:53 +0100

    Code documentation for Query class #6

--- ExShift/Mapping/Query.cs
+++ ExShift/Mapping/Query.cs
@@ -6,6 +6,10 @@ using System.Text.Json;
 
 namespace ExShift.Mapping
 {
+    /// <summary>
+    /// Class for creating search queries.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of object you want to search.</typeparam>
     public class Query<T> where T : IPersistable, new()
     {
         private List<QueryNode> queryNodes;
@@ -15,12 +19,24 @@ namespace ExShift.Mapping
             queryNodes = new List<QueryNode>();
         }
 
+        /// <summary>
+        /// Creates a new <c>Query</c> object.
+        /// </summary>
+        /// <returns>New <c>Query</c> object</returns>
         public static Query<T> Select()
         {
             Query<T> query = new Query<T>();
             return query;
         }
 
+        /// <summary>
+        /// With this method you can specify a search condition. 
+        /// <remarks>
+        /// If you apply a serach condition this method has to be executed first always.
+        /// </remarks>
+        /// </summary>
+        /// <param name="whereClause">Condition for example <c>"property = 'Hello'"</c></param>
+        /// <returns><c>Query</c> object</returns>
         public Query<T> Where(string whereClause)
         {
             QueryNode qn = new QueryNode(whereClause, QueryOperator.ROOT);
@@ -28,6 +44,11 @@ namespace ExShift.Mapping
             return this;
         }
 
+        /// <summary>
+        /// With this method you can specify a search condition with AND operator.
+        /// </summary>
+        /// <param name="expression">Condition for example <c>"property = 2"</c></param>
+        /// <returns><c>Query</c> object</returns>
         public Query<T> And(string expression)
         {
             QueryNode qn = new QueryNode(expression, QueryOperator.AND);
@@ -35,6 +56,11 @@ namespace ExShift.Mapping
             return this;
         }
 
+        /// <summary>
+        /// With this method you can specify a search condition with OR operator.
+        /// </summary>
+        /// <param name="expression">Condition for example <c>"property = 'World'"</c></param>
+        /// <returns></returns>
         public Query<T> Or(string expression)
         {
             QueryNode qn = new QueryNode(expression, QueryOperator.OR);
@@ -42,6 +68,10 @@ namespace ExShift.Mapping
             return this;
         }
 
+        /// <summary>
+        /// Executes the query.
+        /// </summary>
+        /// <returns>Result list</returns>
         public List<T> Run()
         {
             List<T> resultList = new List<T>();
@@ -240,6 +270,13 @@ namespace ExShift.Mapping
             return resultList;
         }
 
+        /// <summary>
+        /// Checks if the search condition from the <see cref="QueryNode"/> 
+        /// applies to the JSON element (serialized object).
+        /// </summary>
+        /// <param name="qn"><see cref="QueryNode"/></param>
+        /// <param name="rawJson">Serialized object as JSON</param>
+        /// <returns><c>True</c> if object fits the search criterion.</returns>
         private bool CheckRawJson(QueryNode qn, string rawJson)
         {
             JsonElement jsonElement = ObjectPackager.DeserializeTupel(rawJson);
@@ -258,6 +295,11 @@ namespace ExShift.Mapping
             return elementIsQualified;
         }
 
+        /// <summary>
+        /// Gets all objects with match the indexed search criterion.
+        /// </summary>
+        /// <param name="qn"><see cref="QueryNode"/></param>
+        /// <returns>Result list</returns>
         private List<T> GetIndexedResults(QueryNode qn)
         {
             List<T> resultList = new List<T>();
