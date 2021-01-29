7cb94339e14540815548054ddea582e35a1b0971
jbaumann2 <jan.baumann2@outlook.com>
2021-01-29 21:50:56 +0100

    Code documentation for PrimaryKey (attribute) class #8

--- ExShift/Mapping/PrimaryKey.cs
+++ ExShift/Mapping/PrimaryKey.cs
@@ -2,6 +2,9 @@
 
 namespace ExShift.Mapping
 {
+    /// <summary>
+    /// Attribute for marking a property as primary key.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = true)]
     public class PrimaryKey : Attribute
     {
