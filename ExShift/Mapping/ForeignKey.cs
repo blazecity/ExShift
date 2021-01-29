fd7e5bd9b079c0ecbf07ca0bab5b270f746c1abb
jbaumann2 <jan.baumann2@outlook.com>
2021-01-29 21:53:30 +0100

    Code documentation for ForeignKey (attribute) class #8

--- ExShift/Mapping/ForeignKey.cs
+++ ExShift/Mapping/ForeignKey.cs
@@ -2,6 +2,9 @@
 
 namespace ExShift.Mapping
 {
+    /// <summary>
+    /// Attribute for marking a property with nested object.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = true)]
     public class ForeignKey : Attribute
     {
