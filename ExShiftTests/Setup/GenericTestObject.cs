0912e31fc7d9c40fba256de2cd1d3c663404cf96
jbaumann2 <jan.baumann2@outlook.com>
2021-06-08 14:39:25 +0200

    new test class for setup

---
+++ ExShiftTests/Setup/GenericTestObject.cs
@@ -0,0 +1,24 @@
+﻿using ExShift.Mapping;
+using System.Collections.Generic;
+
+namespace ExShiftTests.Setup
+{
+    public class GenericTestObjectPT<T> : IPersistable
+    {
+        [PrimaryKey]
+        public int Pk { get; set; }
+
+        [MultiValue]
+        public List<T> List { get; set; }
+    }
+
+    public class GTO<T> : IPersistable
+    {
+        [PrimaryKey]
+        public int Pk { get; set; }
+
+        [MultiValue]
+        [ForeignKey]
+        public List<T> List { get; set; }
+    }
+}
